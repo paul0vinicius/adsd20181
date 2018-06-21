07d24c9fec30276547181b329663650dd0b67a75
Paulo Vinícius Soares <paulo.soares@ccc.ufcg.edu.br>
2018-06-20 23:02:44 -0300

    Modifica o enum

--- src/requests/RequestType.java
+++ src/requests/RequestType.java
@@ -1,8 +1,18 @@
 package requests;
 
 public enum RequestType {
-	GET,
-	PUT,
-	POST,
+	GET(0),
+	PUT(1),
+	POST(2);
 	//DELETE
+	
+	private final int value;
+	
+	private RequestType(int value) {
+		this.value = value;
+	}
+	
+	public int getValue() {
+		return value;
+	}
 }
