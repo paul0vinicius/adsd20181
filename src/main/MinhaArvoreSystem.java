87af9f8ee3dbaf636987867177007547275d9d72
Paulo Vinícius Soares <paulo.soares@ccc.ufcg.edu.br>
2018-06-20 23:03:50 -0300

    Cria main para simulação

---
+++ src/main/MinhaArvoreSystem.java
@@ -0,0 +1,30 @@
+package main;
+
+import eduni.simjava.Sim_system;
+import entities.database.Database;
+import entities.mobile.Mobile;
+import entities.web.Web;
+
+public class MinhaArvoreSystem {
+	
+	public MinhaArvoreSystem() {
+		
+	}
+	
+	public static void main(String[] args) {
+		Sim_system.initialise();
+		
+		Mobile mob = new Mobile("Mobile");
+		Database db = new Database("Database");
+		Web web = new Web("Web");
+		
+		Sim_system.link_ports("Mobile", "Out", "Database", "In");
+		Sim_system.link_ports("Database", "Out", "Mobile", "In");
+		Sim_system.link_ports("Web", "Out", "Database", "In");
+		Sim_system.link_ports("Database", "Out", "Web", "In");
+		
+		
+		Sim_system.run();
+	}
+
+}
